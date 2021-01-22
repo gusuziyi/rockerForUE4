904e55db8bc943bcbb8b33716ccaf9bf6a1a4eee
QMZY <wanwei11@countrygarden.com.cn>
2021-01-22 17:17:58 +0800

    page

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  trailingComma: "es5",
-  tabWidth: 2,
-  singleQuote: true,
-  useTabs: false, // 缩进不使用tab，使用空格
-  semi: true, // 句尾添加分号
-  endOfLine: "lf"
-};
--- config/pages.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  pages: {
-    index: {
-      entry: './src/pages/mobileHome/main.js',
-      template: 'public/index.html',
-      filename: 'index.html',
-    },
-  },
-};
